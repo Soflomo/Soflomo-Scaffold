7e9ca8cce4ad57e8de0555888128ac6e29986a61
Jurian Sluiman <jurian@soflomo.com>
2013-09-20 15:55:29 +0200

    Rename Mockup module to Template module

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,7 @@ return array(
         'Application',
         'Soflomo\Common',
         'Soflomo\Prototype',
-        'Mockup',
+        'Template',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
